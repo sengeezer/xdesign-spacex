0d15f8fc6d64dec0d98a75f0721555b8a43821be
Fabio Albertin <albertin@gmail.com>
2020-10-30 00:58:42 +0000

    Added markup bare skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Spacex-logo */}
+        {/* Reload data */}
       </header>
+      <main>
+        {/* Launch filters */}
+        {/* Launch list */}
+      </main>
+      <footer>
+        <p>All data &copy; SpaceX</p>
+      </footer>
     </div>
   );
 }
