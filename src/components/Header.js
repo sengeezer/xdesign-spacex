92013f6cf0f157a85a149959a14995bb16162890
Fabio Albertin <albertin@gmail.com>
2020-10-31 23:29:48 +0000

    Added missing elements, cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@ import logo from '../assets/spacex-logo.png';
 
 const Header = () => (
   <header className="App-header">
-        <img src={logo} alt="SpaceX Logo" />
-        {/* Reload data */}
-      </header>
+    <img src={logo} alt="SpaceX Logo" className="App-logo" />
+    <h1>Launches</h1>
+    <button>Reload Data ⟲</button>
+  </header>
 );
 
 export default Header;
