dc9e447cde4871d9d1ea0b8d4023abff6414de0a
Fabio Albertin <albertin@gmail.com>
2020-11-02 20:26:56 +0000

    Formatted launch date for display

--- src/components/LaunchListItem.js
+++ src/components/LaunchListItem.js
@@ -1,10 +1,12 @@
+import dayjs from 'dayjs';
+
 const LaunchListItem = ({ launch }) => {
 
   return (
     <li>
       <h2>{launch.flight_number}</h2>
       <h3>{launch.mission_name}</h3>
-      <p>{launch.launch_date_unix}</p>
+      <p>{dayjs.unix(launch.launch_date_unix).format('DD/MM/YYYY')}</p>
       <p>{launch.rocket.rocket_name}</p>
     </li>
   );
