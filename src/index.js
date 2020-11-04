afc7099f00aebb99d1a45cb45920ceca1755f5cf
Fabio Albertin <albertin@gmail.com>
2020-11-04 23:30:25 +0000

    Starting connection of Redux to React app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
 import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { startSetLaunches } from './actions/launches';
+
+const store = configureStore();
+
+const jsx = (
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <p>Loading...</p>,
   document.getElementById('root')
 );
+
+store.dispatch(startSetLaunches()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('root'));
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
