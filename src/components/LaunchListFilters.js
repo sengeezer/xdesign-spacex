a1344ec43ea8be3b63b1c3663ad6b6a28fbc7a37
Fabio Albertin <albertin@gmail.com>
2020-11-05 03:41:07 +0000

    Revamped Main and started adding Redux to and converted LaunchListFilters

--- src/components/LaunchListFilters.js
+++ src/components/LaunchListFilters.js
@@ -1,35 +1,61 @@
-const LaunchListFilters = ({ sortLaunches, resetList }) => {
-  const onSortChange = (e) => {
-    // resetList();
-    sortLaunches('year', e.target.value);
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { sortDesc, sortAsc, filterByYear } from '../actions/filters';
+
+export class LaunchListFilters extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortBy: 'asc'
+    };
+  }
+
+  onSortChange = (e) => {
+    if (e.target.value === 'desc') {
+      this.props.sortDesc();
+    } else {
+      this.props.sortAsc();
+    }
   };
 
-  return (
-    <>
-      <select
-          // value={this.props.filters.sortBy}
-          onChange={onSortChange}
-        >
-        <option value="year">Filter by Year ▾</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-        <option value="2014">2014</option>
-        <option value="2013">2013</option>
-        <option value="2012">2012</option>
-        <option value="2011">2011</option>
-        <option value="2010">2010</option>
-        <option value="2009">2009</option>
-        <option value="2008">2008</option>
-        <option value="2007">2007</option>
-        <option value="2006">2006</option>
-      </select>
-      <button>Sort Descending ⇅</button>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <select
+            // value={this.props.filters.sortBy}
+            onChange={this.onSortChange}
+          >
+          <option value="year">Filter by Year ▾</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+          <option value="2011">2011</option>
+          <option value="2010">2010</option>
+          <option value="2009">2009</option>
+          <option value="2008">2008</option>
+          <option value="2007">2007</option>
+          <option value="2006">2006</option>
+        </select>
+        <button onClick={this.onSortChange}>Sort Descending ⇅</button>
+      </>
+    );
+  }
 };
 
-export default LaunchListFilters;
+const mapDispatchToProps = (dispatch, props) => ({
+  sortDesc: () => dispatch(sortDesc()),
+  sortAsc: () => dispatch(sortAsc()),
+  filterByYear: year => dispatch(filterByYear(year)),
+});
+
+const mapStateToProps = ({ filters }) => ({ filters });
+
+export default connect(mapStateToProps, mapDispatchToProps)(LaunchListFilters);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,58 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import spacex from '../api/spacex';
+// import React, {useState, useEffect} from 'react';
+// import spacex from '../api/spacex';
 import LaunchList from './LaunchList';
 import LaunchListFilters from './LaunchListFilters';
 
 const Main = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [sortMode, setSortMode] = useState('asc');
-  const [launches, setLaunches] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await spacex.get('/launches', {});
-
-      isLoading ? setLaunches(data) : console.log('not loading');
-      
-      setLoading(false);
-    })();
-  }, [launches, isLoading]);
-
-  const resetList = () => {
-    setLoading(true);
-  };
-
-  const sortLaunches = async (sortType, value = null) => {
-    let sortedLaunches;
-
-    switch (sortType) {
-      case 'asc':
-        await resetList();
-        sortedLaunches = await launches.sort((a, b) => a.flight_number < b.flight_number ? 1 : -1);
-
-        setLaunches(sortedLaunches);
-        break;
-      case 'desc':
-        await resetList();
-        sortedLaunches = await launches.sort((a, b) => a.flight_number > b.flight_number ? 1 : -1);
-
-        setLaunches(sortedLaunches);
-        break;
-      case 'year':
-        await resetList();
-        sortedLaunches = await launches.filter(launch => launch.launch_year === value);
-
-        setLaunches(sortedLaunches);
-        break;
-      default:
-        return;
-    }
-  };
-
   return (
     <main>
-      <LaunchListFilters sortLaunches={sortLaunches} resetList={resetList} />
-      <LaunchList launches={launches} />
+      <LaunchListFilters />
+      <LaunchList />
     </main>
   );
 };
