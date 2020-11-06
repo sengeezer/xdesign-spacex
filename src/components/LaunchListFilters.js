c8b2a597acbaf221520716045e6fa032f92b77cd
Fabio Albertin <albertin@gmail.com>
2020-11-06 01:39:15 +0000

    Transformed LaunchListFilters back into functional component

--- src/components/LaunchListFilters.js
+++ src/components/LaunchListFilters.js
@@ -1,61 +1,50 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { sortDesc, sortAsc, filterByYear } from '../actions/filters';
 
-export class LaunchListFilters extends Component {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   sortBy: 'asc'
-    // };
-  }
-
-  onSortChange = (e) => {
-    const { filters } = this.props;
+export const LaunchListFilters = props => {
+  const onSortChange = (e) => {
+    const { filters } = props;
     
     e.preventDefault();
     
     if (filters.sortBy === 'desc') {
-      this.props.sortAsc();
+      props.sortAsc();
     } else {
-      this.props.sortDesc();
+      props.sortDesc();
     }
   };
 
-  onFilterChange = (e) => {
-    this.props.filterByYear(e.target.value)
+  const onFilterChange = (e) => {
+    props.filterByYear(e.target.value)
   };
 
-  render() {
-    return (
-      <>
-        <select
-            // value={this.props.filters.sortBy}
-            onChange={this.onFilterChange}
-          >
-          <option value="year">Filter by Year ▾</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-          <option value="2018">2018</option>
-          <option value="2017">2017</option>
-          <option value="2016">2016</option>
-          <option value="2015">2015</option>
-          <option value="2014">2014</option>
-          <option value="2013">2013</option>
-          <option value="2012">2012</option>
-          <option value="2011">2011</option>
-          <option value="2010">2010</option>
-          <option value="2009">2009</option>
-          <option value="2008">2008</option>
-          <option value="2007">2007</option>
-          <option value="2006">2006</option>
-        </select>
-        <button onClick={this.onSortChange}>Sort {this.props.filters.sortBy === 'desc' ? 'Descending' : 'Ascending'} ⇅</button>
-      </>
-    );
-  }
+  return (
+    <>
+      <select
+        // value={this.props.filters.sortBy}
+        onChange={onFilterChange}
+      >
+        <option value="year">Filter by Year ▾</option>
+        <option value="2020">2020</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+        <option value="2017">2017</option>
+        <option value="2016">2016</option>
+        <option value="2015">2015</option>
+        <option value="2014">2014</option>
+        <option value="2013">2013</option>
+        <option value="2012">2012</option>
+        <option value="2011">2011</option>
+        <option value="2010">2010</option>
+        <option value="2009">2009</option>
+        <option value="2008">2008</option>
+        <option value="2007">2007</option>
+        <option value="2006">2006</option>
+      </select>
+      <button onClick={onSortChange}>Sort {props.filters.sortBy === 'desc' ? 'Descending' : 'Ascending'} ⇅</button>
+    </>
+  );
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
