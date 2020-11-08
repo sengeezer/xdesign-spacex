3281ed61b1c3d449fcc962887880000546a25303
Fabio Albertin <albertin@gmail.com>
2020-11-08 21:20:38 +0000

    Added main image, progressed styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,21 @@
 import LaunchList from './LaunchList';
 import LaunchListFilters from './LaunchListFilters';
 
+import launchHome from '../styles/img/launch-home.png';
+import launchHome2x from '../styles/img/launch-home@2x.png';
+import launchHome3x from '../styles/img/launch-home@3x.png';
+
 const Main = () => {
   return (
     <main>
-      <LaunchListFilters />
-      <LaunchList />
+      <picture>
+        <source srcSet={`${launchHome}, ${launchHome2x} 2x, ${launchHome3x} 3x`} />
+        <img src={launchHome} alt="Background launchpad and rocket" />
+      </picture>
+      <div className="LaunchList-container">
+        <LaunchListFilters />
+        <LaunchList />
+      </div>
     </main>
   );
 };
